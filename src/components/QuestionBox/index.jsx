752d996d66d90943f8d2c50fb6be6457038f5981
emanuelecardone <e_m.cardone@yahoo.it>
2022-07-24 21:26:02 +0100

    Messo mix risposte lodash

--- src/components/QuestionBox/index.jsx
+++ src/components/QuestionBox/index.jsx
@@ -1,22 +1,43 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { useEffect, useState } from 'react';
+import shuffle from 'lodash/shuffle';
+import './style.scss';
 
 const QuestionBox = ({question, index}) => {
 
-    const log = (answer) => (e) => {
+    const checkAnswer = (answer) => (e) => {
         console.log(answer)
     }
 
+    // Crezione array risposte con ordine casuale
+    const allAnswers = [...question.incorrect_answers];
+    allAnswers.push(question.correct_answer)
+
+    const answerBoxes = allAnswers.map((answer, index) => (
+        <div onClick={checkAnswer(answer === question.correct_answer ? 'correct' : 'wrong')} key={answer + index} 
+        className={'single_answer w_40 h_40 d-flex align-items-center justify-content-center border border-3 border-light'}>{answer}</div>
+    ))
+
+    const [answersList, setAnswersList] = useState(answerBoxes);
+
+    useEffect(
+        () => {
+            // Mixed array lodash
+            const mixedArray = shuffle(answerBoxes)
+            setAnswersList(mixedArray);
+            console.log(answersList, question.correct_answer)
+        },
+        []
+    );
+
   return (
     <div className={'w-100 h-100 d-flex flex-column justify-content-between align-items-center question_box_' + index}>
-        <div className={'question_box_up w-100 h_30 d-flex align-items-center text-center'}>
-            <h2>{question.question}</h2>
+        <div className={'question_box_up w-100 h_30 d-flex justify-content-center align-items-center'}>
+            <h2 className='text-center'>{question.question}</h2>
         </div>
-        <div className={'question_box_down w-100 h_80 d-flex flex-wrap'}>
-            <div onClick={log('correct')} className='correct_box w-50 h-50 d-flex align-items-center justify-content-center border border-3 border-light'>{question.correct_answer}</div>
-            {question.incorrect_answers.map((incorrect_answer, index) => (
-                <div onClick={log('wrong')} key={incorrect_answer + index} className='incorrect_box w-50 h-50 d-flex align-items-center justify-content-center border border-3 border-light'>{incorrect_answer}</div>
-            ))}
+        <div className={'question_box_down w-100 h_80 d-flex flex-wrap justify-content-between'}>
+            {answersList}
         </div>
     </div>
   )
